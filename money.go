ef07c8490d166f58fa90012240b241f9a54eb83a
Richard Bone <rjonbone@gmail.com>
2018-07-14 15:16:59 +1000

    Add support for currencies with different formats
    This adds support for parsing currencies with different formats, e.g.
    using ',' as a decimal indicator, '.' as a grouping indicator, or
    having 0 - 3 digits after the decimal indicator.
    
    At the moment this isn't exposed publically as the API is still far
    from stable and I want to figure out the best way to provide sane
    defaults for various currencies and locales. I also need to double
    check what other unusual aspects of currency formats I may not yet
    be handling. Once I've researched that sufficiently I should be able
    to provide a concrete public API.
    
    All going to plan, once the parsing API is stablised I'll look at
    adding formatting support.

--- money.go
+++ money.go
@@ -5,15 +5,26 @@ import "fmt"
 const (
 	stateFirstChar        int8 = 1
 	statePreDecimal       int8 = 2
-	statePostDecimal      int8 = 3
-	stateExpectingDecimal int8 = 4
+	statePreDecimalGroup3 int8 = 3
+	statePreDecimalGroup2 int8 = 4
+	statePreDecimalGroup1 int8 = 5
+	statePostDecimal      int8 = 6
+	stateExpectingDecimal int8 = 7
 )
 
-func ParseAmountForCurrency(amount string, currency Currency) (int64, error) {
-	if currency != USD {
-		return 0, fmt.Errorf("only USD is supported at the moment")
-	}
+type Parser interface {
+	Parse(amount string) (int64, error)
+}
 
+type moneyParser struct {
+	decimalIndicator  rune
+	groupingIndicator rune
+	minorUnits        int64
+}
+
+var usdMoneyParser = moneyParser{decimalIndicator: '.', groupingIndicator: ',', minorUnits: 2}
+
+func (p moneyParser) Parse(amount string) (int64, error) {
 	if len(amount) == 0 {
 		return 0, fmt.Errorf("expected int string, got empty string")
 	}
@@ -62,23 +73,28 @@ func ParseAmountForCurrency(amount string, currency Currency) (int64, error) {
 			}
 		case stateExpectingDecimal:
 			switch char {
-			case '.':
-				if len(amount)-pos != 3 {
-					return 0, fmt.Errorf("insufficient characters after period")
+			case p.decimalIndicator:
+				if int64(len(amount)-pos) != p.minorUnits+1 {
+					return 0, fmt.Errorf("insufficient characters after %q", p.decimalIndicator)
 				} else {
 					state = statePostDecimal
 				}
 			default:
-				return 0, fmt.Errorf("unexpected character %q at position %d, expecting '.'", char, pos)
+				return 0, fmt.Errorf("unexpected character %q at position %d, expecting %q", char, pos, p.decimalIndicator)
 			}
 		case statePreDecimal:
 			switch char {
-			case '.':
-				if len(amount)-pos != 3 {
-					return 0, fmt.Errorf("insufficient characters after period")
+			case p.decimalIndicator:
+				if int64(len(amount)-pos) != p.minorUnits+1 {
+					return 0, fmt.Errorf("insufficient characters after %q", p.decimalIndicator)
 				} else {
 					state = statePostDecimal
 				}
+			case p.groupingIndicator:
+				if len(amount)-pos < 4 {
+					return 0, fmt.Errorf("insufficient characters after %q", p.groupingIndicator)
+				}
+				state = statePreDecimalGroup3
 			case '0':
 				value = value * 10
 			case '1':
@@ -102,6 +118,84 @@ func ParseAmountForCurrency(amount string, currency Currency) (int64, error) {
 			default:
 				return 0, fmt.Errorf("unexpected character %q at position %d", char, pos)
 			}
+		case statePreDecimalGroup3:
+			switch char {
+			case '0':
+				value = value * 10
+			case '1':
+				value = value*10 + 1
+			case '2':
+				value = value*10 + 2
+			case '3':
+				value = value*10 + 3
+			case '4':
+				value = value*10 + 4
+			case '5':
+				value = value*10 + 5
+			case '6':
+				value = value*10 + 6
+			case '7':
+				value = value*10 + 7
+			case '8':
+				value = value*10 + 8
+			case '9':
+				value = value*10 + 9
+			default:
+				return 0, fmt.Errorf("unexpected character %q at position %d", char, pos)
+			}
+			state = statePreDecimalGroup2
+		case statePreDecimalGroup2:
+			switch char {
+			case '0':
+				value = value * 10
+			case '1':
+				value = value*10 + 1
+			case '2':
+				value = value*10 + 2
+			case '3':
+				value = value*10 + 3
+			case '4':
+				value = value*10 + 4
+			case '5':
+				value = value*10 + 5
+			case '6':
+				value = value*10 + 6
+			case '7':
+				value = value*10 + 7
+			case '8':
+				value = value*10 + 8
+			case '9':
+				value = value*10 + 9
+			default:
+				return 0, fmt.Errorf("unexpected character %q at position %d", char, pos)
+			}
+			state = statePreDecimalGroup1
+		case statePreDecimalGroup1:
+			switch char {
+			case '0':
+				value = value * 10
+			case '1':
+				value = value*10 + 1
+			case '2':
+				value = value*10 + 2
+			case '3':
+				value = value*10 + 3
+			case '4':
+				value = value*10 + 4
+			case '5':
+				value = value*10 + 5
+			case '6':
+				value = value*10 + 6
+			case '7':
+				value = value*10 + 7
+			case '8':
+				value = value*10 + 8
+			case '9':
+				value = value*10 + 9
+			default:
+				return 0, fmt.Errorf("unexpected character %q at position %d", char, pos)
+			}
+			state = statePreDecimal
 		case statePostDecimal:
 			switch char {
 			case '0':
@@ -132,12 +226,25 @@ func ParseAmountForCurrency(amount string, currency Currency) (int64, error) {
 		}
 	}
 
-	if state == statePreDecimal {
-		value = value * 100
+	switch state {
+	case statePreDecimalGroup3, statePreDecimalGroup2, statePreDecimalGroup1:
+		return 0, fmt.Errorf("insufficient digits after %q", p.groupingIndicator)
+	case statePreDecimal:
+		for i := int64(0); i < p.minorUnits; i++ {
+			value = value * 10
+		}
 	}
 
 	if amount[0] == '-' {
 		value = value * -1
 	}
 	return value, nil
+}
+
+func ParseAmountForCurrency(amount string, currency *Currency) (int64, error) {
+	if currency != USD {
+		return 0, fmt.Errorf("only USD is supported at the moment")
+	}
+
+	return usdMoneyParser.Parse(amount)
 }
